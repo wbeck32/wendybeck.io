5dcadeb613f577974a2bdf1d31d1fac4725eb02d
Wendy Beck <webeck@gmail.com>
2017-10-12 15:53:45 -0700

    adding copy script

--- src/sites/SiteContainer.js
+++ src/sites/SiteContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Card';
 import siteData from './siteData';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 export default class SiteContainer extends Component {
   constructor() {
